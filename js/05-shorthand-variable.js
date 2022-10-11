e8821b52cb2a7c42f6f3793bfe8f9258e2ec1e48
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 19:04:09 +0530

    Two variable are declared and assigned value in one line and other variable in next line

--- js/05-shorthand-variable.js
+++ js/05-shorthand-variable.js
@@ -4,7 +4,12 @@
 // let total = price * quantity;
 
 // Declaring variables on the same line and values are assigned to each
-let price, quantity, total;
-price = 5;
-quantity = 14;
-total = price * quantity;
+// let price, quantity, total;
+// price = 5;
+// quantity = 14;
+// total = price * quantity;
+
+// Two variable are declared and assigned value in one line and other variable in next line
+let price = 5,
+  quantity = 14;
+let total = price * quantity;
