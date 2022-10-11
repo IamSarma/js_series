88855f52305ff573585127a304e31fcc14d87306
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 21:35:33 +0530

    Concatenate the variables to create the welcome message

--- js/12-basic-javascript-instructions.js
+++ js/12-basic-javascript-instructions.js
@@ -2,3 +2,6 @@
 let greeting = "Howdy ";
 let username = "John";
 let message = ", please check your order:";
+
+// Concatenate the variables to create the welcome message
+let welcomeMessage = greeting + username + message;
