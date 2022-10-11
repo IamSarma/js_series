b95ee4468f12e492ccc284f925780fbb0cc64db9
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 19:05:44 +0530

    Shorthand variable - Write total into the element with id of cost

--- js/05-shorthand-variable.js
+++ js/05-shorthand-variable.js
@@ -1,7 +1,7 @@
 // Declaring variables and assigning values in the same line
-// let price = 5;
-// let quantity = 14;
-// let total = price * quantity;
+let price = 5;
+let quantity = 14;
+let total = price * quantity;
 
 // Declaring variables on the same line and values are assigned to each
 // let price, quantity, total;
@@ -10,6 +10,10 @@
 // total = price * quantity;
 
 // Two variable are declared and assigned value in one line and other variable in next line
-let price = 5,
-  quantity = 14;
-let total = price * quantity;
+// let price = 5,
+//   quantity = 14;
+// let total = price * quantity;
+
+// Write total into the element with id of cost
+let leCost = document.getElementById("cost");
+leCost.textContent = "$" + total;
