26001f3ff3987027185bb9db208d7c24ebd6c881
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 19:03:10 +0530

    Declaring variables on the same line and values are assigned to each

--- js/05-shorthand-variable.js
+++ js/05-shorthand-variable.js
@@ -1,4 +1,10 @@
 // Declaring variables and assigning values in the same line
-let price = 5;
-let quantity = 14;
-let total = price * quantity;
+// let price = 5;
+// let quantity = 14;
+// let total = price * quantity;
+
+// Declaring variables on the same line and values are assigned to each
+let price, quantity, total;
+price = 5;
+quantity = 14;
+total = price * quantity;
