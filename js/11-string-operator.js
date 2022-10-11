81e2863caee22689165df8032d3511ee3f015219
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 21:23:23 +0530

    String operator

---
+++ js/11-string-operator.js
@@ -0,0 +1,7 @@
+let greeting = "Howdy ";
+let username = "Rowdy";
+
+let welcomeMessage = greeting + username + "!";
+
+var elGreeting = document.getElementById("greeting");
+elGreeting.textContent = welcomeMessage;
