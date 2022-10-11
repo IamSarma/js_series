b61aced389ce6794bed4d155ddb7e33dbdacec69
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 22:11:44 +0530

    Basic function

---
+++ js/13-basic-function.js
@@ -0,0 +1,8 @@
+let message = "Sign up to receive our newsletter for 10% off!";
+
+function updateMessage() {
+  let elMessage = document.getElementById("message");
+  elMessage.textContent = message;
+}
+
+updateMessage();
