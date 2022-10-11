777593810cf0fa5b9a293086b031266310f55195
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 18:56:57 +0530

    Boolean variable

---
+++ js/04-boolean-variable.js
@@ -0,0 +1,11 @@
+let inStock;
+let shipping;
+
+inStock = true;
+shipping = false;
+
+let elStock = document.getElementById("stock");
+elStock.className = inStock;
+
+let elShipping = document.getElementById("shipping");
+elShipping.className = shipping;
