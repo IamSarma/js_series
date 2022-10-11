9dbf9e1e49055061cfbace8d5a16dbb2f2609027
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 18:45:50 +0530

    String with quotes

---
+++ js/03-string-with-quotes.js
@@ -0,0 +1,11 @@
+let title;
+let message;
+
+title = "John's Special Offers";
+message = '<a href="#">25% off!</a>';
+
+var elTitle = document.getElementById("title");
+elTitle.innerHTML = title;
+
+var elNote = document.getElementById("note");
+elNote.innerHTML = message;
