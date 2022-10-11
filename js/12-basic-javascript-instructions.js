a377e178f192e39b4605104e3563baa4c467d602
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 21:37:32 +0530

    Create variales to hold the details about the sign

--- js/12-basic-javascript-instructions.js
+++ js/12-basic-javascript-instructions.js
@@ -5,3 +5,10 @@ let message = ", please check your order:";
 
 // Concatenate the variables to create the welcome message
 let welcomeMessage = greeting + username + message;
+
+// Create variales to hold the details about the sign
+let sign = "John Wick House";
+let tiles = sign.length;
+let subTotal = tiles * 5;
+let shipping = 7;
+let grandTotal = subTotal + shipping;
