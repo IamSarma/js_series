ec8c7bf8216740cbe366269875e6a99f7f745f1a
IamSarma <sarmaakondivnm@gmail.com>
2022-10-11 21:39:30 +0530

    Get the element that has an id of greeting and replace the content with welcome message

--- js/12-basic-javascript-instructions.js
+++ js/12-basic-javascript-instructions.js
@@ -12,3 +12,7 @@ let tiles = sign.length;
 let subTotal = tiles * 5;
 let shipping = 7;
 let grandTotal = subTotal + shipping;
+
+// Get the element that has an id of greeting and replace the content with welcome message
+let elGreeting = document.getElementById("greeting");
+elGreeting.textContent = welcomeMessage;
