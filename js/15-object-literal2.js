7dc654bed05acce0979229950d6e3cb2f1798795
IamSarma <sarmaakondivnm@gmail.com>
2022-10-12 09:53:28 +0530

    Object literal2

---
+++ js/15-object-literal2.js
@@ -0,0 +1,14 @@
+let hotel = {
+  name: "Park",
+  rooms: 120,
+  booked: 77,
+  checkAvailability: function () {
+    return this.rooms - this.booked;
+  },
+};
+
+let elName = document.getElementById("hotelName");
+elName.textContent = hotel.name;
+
+let elRooms = document.getElementById("rooms");
+elRooms.textContent = hotel.checkAvailability();
