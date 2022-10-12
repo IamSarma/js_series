fb03bb4beed4acecd2bb19b10e60e267a9f72bf7
IamSarma <sarmaakondivnm@gmail.com>
2022-10-12 13:10:56 +0530

    Multiple objects

--- js/17-multiple-objects.js
+++ js/17-multiple-objects.js
@@ -6,3 +6,18 @@ function Hotel(name, rooms, booked) {
     return this.rooms - this.booked;
   };
 }
+
+let quayHotel = new Hotel("Quay", 40, 25);
+let parkHotel = new Hotel("Park", 120, 77);
+
+let quayDetails = quayHotel.name + " rooms: ";
+quayDetails += quayHotel.checkAvailability();
+
+let parkDetails = parkHotel.name + " rooms: ";
+parkDetails += parkHotel.checkAvailability();
+
+let elHotel1 = document.getElementById("hotel1");
+elHotel1.textContent = quayDetails;
+
+let elHotel2 = document.getElementById("hotel2");
+elHotel2.textContent = parkDetails;
