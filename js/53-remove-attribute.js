67238645853a58a988054cc5f78b03fcc2630c6e
IamSarma <sarmaakondivnm@gmail.com>
2022-10-15 22:22:58 +0530

    Remove attribute

---
+++ js/53-remove-attribute.js
@@ -0,0 +1,7 @@
+// Get the first item
+let firstItem = document.getElementById("one");
+
+// Remove its class attribute if it exist
+if (firstItem.hasAttribute("class")) {
+  firstItem.removeAttribute("class");
+}
