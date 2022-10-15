8eddc746016999e99ec4e91d89dd3aa47fc30d28
IamSarma <sarmaakondivnm@gmail.com>
2022-10-15 22:16:46 +0530

    Set attribute

---
+++ js/52-set-attribute.js
@@ -0,0 +1,11 @@
+// Get the first item
+let firstItem = document.getElementById("one");
+
+// Change its class attribute
+firstItem.className = "complete";
+
+// Get fourth item
+let fourthItem = document.getElementsByTagName("li")[3];
+
+// Add an attribute to it
+fourthItem.setAttribute("class", "cool");
