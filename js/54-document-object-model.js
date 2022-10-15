90fad73eb1b547c0266ccacb5cac7ca7b98e8e7c
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 00:29:10 +0530

    Add new item to end of list

--- js/54-document-object-model.js
+++ js/54-document-object-model.js
@@ -1,3 +1,9 @@
 // Adding items to start and end of list
 // Get the ul element
 let list = document.getElementsByTagName("ul")[0];
+
+// Add new item to end of list
+let newItemLast = document.createElement("li");
+let newTextLast = document.createTextNode("cream");
+newItemLast.appendChild(newTextLast);
+list.appendChild(newItemLast);
