579d946ccefa6445d54bfb2f11d3084fa940223f
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 21:25:16 +0530

    Mutation - Declare variables and store elements

---
+++ js/67-mutation.js
@@ -0,0 +1,6 @@
+// Declare variables and store elements
+let elList, addLink, newEl, newText, counter, listItems;
+
+elList = document.getElementById("list");
+addLink = document.querySelector("a");
+counter = document.getElementById("counter");
