a36899f70abb68e9e1a9a074b6aeb11c2b0f79bc
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 17:35:37 +0530

    Form - packageHint function

--- js/66-form.js
+++ js/66-form.js
@@ -6,6 +6,16 @@ elPackageHint = document.getElementById("packageHint");
 elTerms = document.getElementById("terms");
 elTermsHint = document.getElementById("termsHint");
 
+// packageHint function
+function packageHint() {
+  let package = this.options[this.selectedIndex].value;
+  if (package == "monthly") {
+    elPackageHint.innerHTML = "Save $10 if you pay for 1 year!";
+  } else {
+    elPackageHint.innerHTML = "Wise choice!";
+  }
+}
+
 // checkTerms function
 function checkTerms(event) {
   if (!elTerms.checked) {
