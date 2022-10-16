69950394f9b3bf5a75896e3c36e0c81a5db1c374
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 21:25:57 +0530

    Mutation - Add event listeners

--- js/67-mutation.js
+++ js/67-mutation.js
@@ -4,3 +4,7 @@ let elList, addLink, newEl, newText, counter, listItems;
 elList = document.getElementById("list");
 addLink = document.querySelector("a");
 counter = document.getElementById("counter");
+
+// Add event listeners
+addLink.addEventListener("click", addItem, false);
+elList.addEventListener("DOMNodeInserted", updateCount, false);
