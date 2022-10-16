047cae3c9b5e98630ee3d4d448c88015fa7e78ff
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 08:40:08 +0530

    Event Listener With Parameters

---
+++ js/57-event-listener-with-parameters.js
@@ -0,0 +1,18 @@
+let elUsername = document.getElementById("username");
+let elMsg = document.getElementById("feedback");
+
+function checkUsername(minLength) {
+  if (elUsername.value.length < minLength) {
+    elMsg.textContent = "Username must be " + minLength + " characters or more";
+  } else {
+    elMsg.textContent = "";
+  }
+}
+
+elUsername.addEventListener(
+  "blur",
+  function () {
+    checkUsername(5);
+  },
+  false
+);
