2580dd181b453b9220827f135d3c820f1d82bb98
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 17:28:28 +0530

    Form - Declare variables and store elements

---
+++ js/66-form.js
@@ -0,0 +1,7 @@
+// Declare variables and store elements
+let elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;
+elForm = document.getElementById("formSignup");
+elSelectPackage = document.getElementById("package");
+elPackageHint = document.getElementById("packageHint");
+elTerms = document.getElementById("terms");
+elTermsHint = document.getElementById("termsHint");
