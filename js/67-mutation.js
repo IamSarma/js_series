8c3b70c7c590b727c525bc24c52a498e358b6f2f
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 21:26:27 +0530

    Mutation - addItem function

--- js/67-mutation.js
+++ js/67-mutation.js
@@ -5,6 +5,15 @@ elList = document.getElementById("list");
 addLink = document.querySelector("a");
 counter = document.getElementById("counter");
 
+// addItem function
+function addItem(e) {
+  e.preventDefault();
+  newEl = document.createElement("li");
+  newText = document.createTextNode("New list item");
+  newEl.appendChild(newText);
+  elList.appendChild(newEl);
+}
+
 // Add event listeners
 addLink.addEventListener("click", addItem, false);
 elList.addEventListener("DOMNodeInserted", updateCount, false);
