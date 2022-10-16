8bd11bc8e2a7229d6efe9dbb7e833e4cac4a5c3d
IamSarma <sarmaakondivnm@gmail.com>
2022-10-16 17:31:55 +0530

    Form - checkTerms function

--- js/66-form.js
+++ js/66-form.js
@@ -6,6 +6,14 @@ elPackageHint = document.getElementById("packageHint");
 elTerms = document.getElementById("terms");
 elTermsHint = document.getElementById("termsHint");
 
+// checkTerms function
+function checkTerms(event) {
+  if (!elTerms.checked) {
+    elTermsHint.innerHTML = "You must agree to the terms.";
+    event.preventDefault();
+  }
+}
+
 // Create event listeners
 elForm.addEventListener("submit", checkTerms, false);
 elSelectPackage.addEventListener("change", packageHint, false);
