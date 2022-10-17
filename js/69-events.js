d9c5cfecdc1d406c624d716619fa93aff02b2abf
IamSarma <sarmaakondivnm@gmail.com>
2022-10-17 08:26:36 +0530

    Events - Declaring variables and storing elements

---
+++ js/69-events.js
@@ -0,0 +1,5 @@
+// Declaring variables and storing elements
+let noteInput, noteName, textEneterd, target;
+
+noteName = document.getElementById("noteName");
+noteInput = document.getElementById("noteInput");
