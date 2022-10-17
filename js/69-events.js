ad9789b02a5ef4bcbdce7a1b3ce6f4c1661ea367
IamSarma <sarmaakondivnm@gmail.com>
2022-10-17 08:33:24 +0530

    Events - recordContorls function

--- js/69-events.js
+++ js/69-events.js
@@ -4,6 +4,24 @@ let noteInput, noteName, textEneterd, target;
 noteName = document.getElementById("noteName");
 noteInput = document.getElementById("noteInput");
 
+// recordContorls function
+function recordContorls(e) {
+  if (!e) {
+    e = window.event;
+  }
+  target = e.target;
+  e.preventDefault();
+
+  switch (target.getAttribute("data-state")) {
+    case "record":
+      record(target);
+      break;
+    case "stop":
+      stop(target);
+      break;
+  }
+}
+
 // Adding event listeners
 document.addEventListener(
   "click",
